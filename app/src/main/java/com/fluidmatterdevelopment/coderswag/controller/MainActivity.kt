8355612329bcb7293bda218a77b0b47c311aa47a
Jordan <jcrerar@gmail.com>
2019-09-28 20:16:26 -0400

    finsihed custom addapter

--- app/src/main/java/com/fluidmatterdevelopment/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/fluidmatterdevelopment/coderswag/controller/MainActivity.kt
@@ -2,24 +2,21 @@ package com.fluidmatterdevelopment.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import com.fluidmatterdevelopment.coderswag.R
-import com.fluidmatterdevelopment.coderswag.model.Catagory
+import com.fluidmatterdevelopment.coderswag.adapters.CategoryAdapter
 import com.fluidmatterdevelopment.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     //lateinit vars/vals
-    lateinit var adapter: ArrayAdapter<Catagory>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         lvCategory.adapter = adapter
     }
 }
