537a576b89efdeb903e8be523d89a0a13aec5b75
Jordan <jcrerar@gmail.com>
2019-10-01 17:02:31 -0400

    prepare for Recycler view addapter

--- app/src/main/java/com/fluidmatterdevelopment/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/fluidmatterdevelopment/coderswag/controller/MainActivity.kt
@@ -2,12 +2,13 @@ package com.fluidmatterdevelopment.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.fluidmatterdevelopment.coderswag.R
 import com.fluidmatterdevelopment.coderswag.adapters.CategoryAdapter
 import com.fluidmatterdevelopment.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class  MainActivity : AppCompatActivity() {
 
     //lateinit vars/vals
     lateinit var adapter: CategoryAdapter
@@ -17,6 +18,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         adapter = CategoryAdapter(this, DataService.categories)
-        lvCategory.adapter = adapter
+        rvCategory.adapter = adapter
+
+        rvCategory.setOnItemClickListener { adapterView, view, i, l ->
+            val category = DataService.categories[i]
+            Toast.makeText(this, "You Clicked on the ${category.title} cell",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 }
